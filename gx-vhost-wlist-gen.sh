4027b2a027c976778f79a1306a642fd9fae7b9c1
geexirooz <roozbehjamali1995@gmail.com>
2025-03-07 18:54:19 +0000

    add wlist generator for virtual hosts

---
+++ gx-vhost-wlist-gen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Input
+DOMAIN="$1"
+OUTPUT_DIR="$2"
+
+# gxsub.sh script stuff
+GXENUM_DIR="$HOME/.gxenum"
+SUBS_FILE="$GXENUM_DIR/$DOMAIN.subs"
+
+# Files create by this script
+ALL_VHOSTS_FILE="$OUTPUT_DIR/vhosts-all.txt"
+PURE_VHOSTS_FILE="$OUTPUT_DIR/vhosts-pure.txt"
+
+# Tools absolute paths
+ANEW="$HOME/go/bin/anew"
+
+if [[ ! -d "$OUTPUT_DIR" ]]; then
+    /usr/bin/mkdir -p "$OUTPUT_DIR"
+fi
+
+/usr/bin/cat $SUBS_FILE | sed "s/.$DOMAIN//g" | $ANEW $ALL_VHOSTS_FILE | tr '.' '\n' | $ANEW $PURE_VHOSTS_FILE | $ANEW -q $ALL_VHOSTS_FILE 
+/usr/bin/cat $PURE_VHOSTS_FILE | tr '-' '\n' | $ANEW $PURE_VHOSTS_FILE | $ANEW -q $ALL_VHOSTS_FILE
+
