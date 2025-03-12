b523608bc0ac6d408d1cbece29efee5ab82761d7
geexirooz <roozbehjamali1995@gmail.com>
2025-03-12 19:17:51 +0000

    remove http deduper from endext-cleaner

--- gx-endext-cleaner.py
+++ gx-endext-cleaner.py
@@ -20,26 +20,8 @@ for line in sys.stdin:
     if path.startswith("/"):
         # Path starts with a slash, append it to the base URL
         full_url = base_url + path
-        url_key = parsed_url.netloc + path #full_url without scheme
-        url_scheme = parsed_url.scheme
     else:
         # Path does not start with a slash, treat it as a complete URL
         full_url = org_url + path
-        url_key = parsed_url.netloc + parsed_url.path + path #full_url without scheme
-        url_scheme = parsed_url.scheme
 
-    if url_key not in url_dict:
-        url_dict[url_key] = url_scheme
-    else:
-        # If 'http' is found but 'https' is already present, ignore 'http'
-        if url_dict[url_key] == 'https':
-            continue
-        # Otherwise, keep the first encounter (which is either http or https)
-        elif url_dict[url_key] == 'http' and url_scheme == 'https':
-            url_dict[url_key] = 'https'
-        else:
-            continue
-
-# Output the result
-for url, scheme in url_dict.items():
-    print(scheme + "://" + url)
+    print(full_url)
